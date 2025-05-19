15720e9fb481a7ceedb2cd3a055b23131f5d2fe5
jai847 <jaichauhan8475030@gmail.com>
2025-05-19 14:44:14 +0530

    Update MAIN.py (assignment_2)

--- MAIN.PY.py
+++ MAIN.PY.py
@@ -1,9 +1,13 @@
+#task 1
+
 a=int(input("enter the value:"))
 b=int(input("enter the value:"))
 c= a+b
 print(c)
 
-a=input("enter the value:")
-b=input("enter the value:")
-c=a+b
-print(c)
+#task 2
+
+name_1=input("enter the name:")
+name_2=input("enter the name:")
+name=name_1+name_2
+print(name)
