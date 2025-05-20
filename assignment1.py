c7c5c130feda054a3cf6cc54cff4e2779a603638
jai847 <jaichauhan8475030@gmail.com>
2025-05-20 14:40:24 +0530

    main.py (assignment-1)

---
+++ assignment1.py
@@ -0,0 +1,33 @@
+#task 1- Perform Basic Mathematical Operations
+
+
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+add= a+b
+print(add)
+
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+subtract=a-b
+print(subtract)
+
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+multiply= a*b
+print(multiply)
+
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+divide= a/b
+print(divide)
+
+
+#task 2-Create a Personalized Greeting
+
+
+Name_1=input("enter the frsit name:")
+Name_2=input("enter the last name:")
+Name= Name_1+Name_2
+print(Name)
+
+
